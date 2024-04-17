7fb051222a0baf8708d78739eb682dca4bf5338e
chronosgit <warfacemeister@mail.ru>
2024-04-17 22:00:37 +0500

    feat: changed categories from static to state

--- src/modules/catalogue/Catalogue.jsx
+++ src/modules/catalogue/Catalogue.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import Categories from "@common/components/categories/Categories";
 import Logo from "@common/components/logo/Logo";
-import categories from "./api/mockCategories";
+import ButtonOrder from "@common/components/button_order/ButtonOrder";
+import mockStaticCategories from "./api/mockCategories";
 import styles from "./catalogue.module.css";
 
 const Catalogue = () => {
+
+	// TODO: add cart states
+	const [categories, setCategories] = useState(mockStaticCategories);
 
 	return (
 		<div id="catalogue" className={styles.catalogue}>
 			<div className={styles.logo_wrapper}>
 				<Logo width="3.5rem" />
+			</div>
+
+			<div className={styles.button_order_wrapper}>
+				<ButtonOrder text="Временный текст" />
 			</div>
 
 			<Categories categories={categories} />
