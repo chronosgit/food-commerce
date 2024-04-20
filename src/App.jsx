25055c5a92e521014d12eb1da64f08c685ffd8f7
chronosgit <warfacemeister@mail.ru>
2024-04-20 21:05:10 +0500

    feat: put cart module main comp. into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Cart from "@modules/cart/Cart";
 import { CartContextProvider } from "@common/contexts/CartContext";
+import useCart from "@common/hooks/useCart";
 import routes from "@routes/routes";
 
 const App = () => {
 
   const Router = createBrowserRouter(routes);
 
+  const {isCartOpen, closeCart} = useCart();
+
   return (
     <CartContextProvider>
       <RouterProvider router={Router} />
+
+      <Cart 
+        isVisible={isCartOpen} 
+        onClose={closeCart}
+      />
     </CartContextProvider>
   );
 };
