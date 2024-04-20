fbb403f6bcf3999969ad345c9a4b6eb38e1eee99
chronosgit <warfacemeister@mail.ru>
2024-04-20 21:34:59 +0500

    feat: started making cart UI

--- src/modules/cart/Cart.jsx
+++ src/modules/cart/Cart.jsx
@@ -1,10 +1,21 @@
+import { useContext } from "react";
+import CartContext from "@common/contexts/CartContext";
 import styles from "./cart.module.css";
 
 const Cart = () => {
+	
+	const {isCartOpen, closeCart} = useContext(CartContext);
+
+	const closedCartStyles = {
+		transform: isCartOpen ? "translateX(0%)" : "translateX(150%)",
+	};
 
 	return (
-		<div className={styles.window}>
-			Cart
+		<div 
+			className={styles.window}
+			style={closedCartStyles}
+		>
+			<button onClick={closeCart}>Close</button>
 		</div>
 	);
 };
