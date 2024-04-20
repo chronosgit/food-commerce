1090965e80968bc6183cb3bcc6aaa6aac446dce3
chronosgit <warfacemeister@mail.ru>
2024-04-20 21:17:18 +0500

    feat: removed cart module comp. from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Cart from "@modules/cart/Cart";
 import { CartContextProvider } from "@common/contexts/CartContext";
-import useCart from "@common/hooks/useCart";
 import routes from "@routes/routes";
 
 const App = () => {
 
   const Router = createBrowserRouter(routes);
 
-  const {isCartOpen, closeCart} = useCart();
-
   return (
     <CartContextProvider>
       <RouterProvider router={Router} />
 
-      <Cart 
-        isVisible={isCartOpen} 
-        onClose={closeCart}
-      />
+      <Cart />
     </CartContextProvider>
   );
 };
