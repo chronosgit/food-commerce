d1fbe2c297512e7873294e8cec90eae12096cce0
chronosgit <warfacemeister@mail.ru>
2024-04-20 20:15:37 +0500

    feat: coupled cart logic to product component

--- src/common/components/product/Product.jsx
+++ src/common/components/product/Product.jsx
@@ -1,17 +1,33 @@
+import { useContext, useState } from "react";
 import formatToTengePrice from "@common/utils/formatToTengePrice";
 import QuantitySelector from "@common/components/quantity_selector/QuantitySelector";
+import CartContext from "@common/contexts/CartContext";
+import imageOnError from "@common/utils/imageOnError";
 import fallbackImage from "@assets/images/placeholder.png";
 import styles from "./product.module.css";
 
 const Product = ({product: prod}) => {
 
+	const {
+		addToCart, removeFromCart,
+	} = useContext(CartContext);
+
+	const [pickedQuantity, setPickedQuantity] = useState(0);
+
 	const price = formatToTengePrice(prod.price);
 
-	const imageOnError = (e) => {
-		// NOTE: prevent infinite error loop
-		e.target.onError = null;
+	const addProduct = () => {
+		setPickedQuantity(prev => prev + 1);
+		addToCart(prod);
+	};
 
-		e.target.src = fallbackImage;
+	const removeProduct = () => {
+		if(pickedQuantity === 0) {
+			return;
+		}
+
+		setPickedQuantity(prev => prev - 1);
+		removeFromCart(prod.id);
 	};
 
 	return (
@@ -20,7 +36,7 @@ const Product = ({product: prod}) => {
 				src={prod.imageUrl} 
 				alt="" 
 				className={styles.product_img}
-				onError={(e) => imageOnError(e)}/
+				onError={(e) => imageOnError(e, fallbackImage)}/
 			>
 
 			<div className={styles.product_content}>
@@ -39,7 +55,12 @@ const Product = ({product: prod}) => {
 				</p>
 
 				<div className={styles.quantity_selector_wrapper}>
-					<QuantitySelector />
+					<QuantitySelector 
+						quantity={pickedQuantity}
+						isMinusDisabled={pickedQuantity === 0} 
+						onPlus={addProduct}
+						onMinus={removeProduct}
+					/>
 				</div>
 			</div>
 		</div>
