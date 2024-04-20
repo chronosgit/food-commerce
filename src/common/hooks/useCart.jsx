8dee547d2c807b7b5398696be16e45a012b4918e
chronosgit <warfacemeister@mail.ru>
2024-04-20 20:30:20 +0500

    feat: updated common useCart hook

--- src/common/hooks/useCart.jsx
+++ src/common/hooks/useCart.jsx
@@ -3,17 +3,38 @@ import { useState } from "react";
 const useCart = () => {
 
 	const [cartProducts, setCardProducts] = useState([]);
+	const [totalPrice, setTotalPrice] = useState(0);
 
 	const addToCart = (product) => {
-		console.log("Added: ", product);
+		setTotalPrice(prev => prev + product.price);
+
+		setCardProducts(prev => {
+			return [
+				...prev,
+				product,
+			];
+		});
 	};
 
-	const removeFromCart = (productId) => {
-		console.log("Removed by id: ", productId);
+	const removeFromCart = (productId, productPrice) => {
+		let isOneMarkedFalse = false;
+
+		const updatedCartProds = cartProducts.filter(cp => {
+			if(!isOneMarkedFalse && cp.id === productId) {
+				isOneMarkedFalse = true;
+				return false;
+			}
+
+			return true;
+		});
+
+		setTotalPrice(prev => prev - productPrice);
+		setCardProducts(updatedCartProds);
 	};
 
 	return {
-		cartProducts, addToCart, removeFromCart,
+		cartProducts, totalPrice,
+		addToCart, removeFromCart,
 	};
 };
 
